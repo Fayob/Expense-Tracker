72bc59009247d7b69f1e128ace1255ad8bbd2524
fayob <adedejifavour6@gmail.com>
2023-12-07 07:25:30 +0100

    Add All expensive screen

---
+++ screens/AllExpenses.js
@@ -0,0 +1,8 @@
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput"
+
+
+function AllExpenses() {
+  return <ExpensesOutput expensesPeriod="Total" />
+}
+
+export default AllExpenses
