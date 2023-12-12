d1fbb2b2f23f7911445b753fb142d8c37c16e481
fayob <adedejifavour6@gmail.com>
2023-12-12 10:39:51 +0100

    Add functionality to fetch data from the backend

--- util/http.js
+++ util/http.js
@@ -9,3 +9,20 @@ export async function storeExpense(expenseData) {
   return id;
 }
 
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + '/expenses.json');
+
+  const expenses = []
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description
+    }
+    expenses.push(expenseObj)
+  }
+
+  return expenses
+}
